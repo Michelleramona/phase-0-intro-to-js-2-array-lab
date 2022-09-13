7e4a892f9b4bed67a0f8ead9c0b2e10f80fba58e
Michelle Vitello <michelleramona89@gmail.com>
2022-09-13 06:43:28 -0400

    commit tree a78d158578a324db445330bd29133a4fd4c4b867

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.splice(-1)
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.splice(0,1)
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
